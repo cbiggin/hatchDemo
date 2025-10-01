a2b751ee4c992942f7803fde7c92de84ff215d43
Colin Biggin <cbiggin@mac.com>
2025-10-01 14:46:03 -0400

    refactor: basic trigger of download of manifest

--- hatchDemo/ViewControllers/ScrollingVC.swift
+++ hatchDemo/ViewControllers/ScrollingVC.swift
@@ -9,8 +9,31 @@ import UIKit
 
 class ScrollingVC: UIViewController {
 
+	private var videoUrls: [String] = []
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		Task { await self.downloadVideos() }
 	}
+}
+
+// MARK: IBActions
+extension ScrollingVC {
+
+	private func downloadVideos() async {
+		let api = API()
+		do {
+			let videos = try await api.downloadManifest("https://cdn.dev.airxp.app/AgentVideos-HLS-Progressive/manifest.json")
+			videoUrls = videos
+			redoTableView()
+		} catch {
+			print("Failed to download manifest: \(error)")
+		}
+	}
+
+	private func redoTableView() {
+		// coming soon
+	}
+
 }
 
